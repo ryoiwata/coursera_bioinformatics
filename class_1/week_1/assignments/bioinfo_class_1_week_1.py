e5bdbcddf051c1b60311e5387ba21bb21bd0cdb8
ryoi360 <ryoi360@gmail.com>
2019-07-25 11:16:36 -0500

    pattern matching function

--- class_1/week_1/assignments/bioinfo_class_1_week_1.py
+++ class_1/week_1/assignments/bioinfo_class_1_week_1.py
@@ -5,6 +5,19 @@ def reverse_compliment(input_string):
         result += compliment_dict[nucleotide]
     return result
 
+def pattern_matching(pattern, genome):
+    result = []
+    result_str = ""
+    for index in range(len(genome) - 1):
+        if genome[index:index + len(pattern)] == pattern:
+            result.append(index)
+            result_str += str(index) + " "
+    # return result_str
+    return result
+    
 
+pattern = "CTTGATCAT"
+genome = open("../data/Vibrio_cholerae.txt")
+genome_str = genome.read()
 
-print(reverse_compliment("ACGT"))
+print(pattern_matching(pattern, genome_str))
